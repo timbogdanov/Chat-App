d08bce713f43dc2072b67dab893dcf606a942f01
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-09-25 16:14:54 -0700

    fix endpoint

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
 
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://react-chap-app.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
